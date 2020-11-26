86034ffec3e01659948403e098d7e3ebe63aa99c
Aleksey Ponomarev <dazed39@mail.ru>
2020-11-26 22:29:34 +0200

    first_test

--- send.php
+++ send.php
@@ -10,13 +10,13 @@ $ip = $_SERVER['REMOTE_ADDR'];
 
 // Post request to server:
 
-$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey . '&response=' . $captcha;
+$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey . '&response=' . $captcha . '&remoteip=' . $ip;
 
 $response = file_get_contents($url);
 $responseKeys = json_decode($response, true);
 header('Content-type: application/json');
 
-if($responseKeys["sucess"] && $responseKeys["score"] >= 0.5) {
+if($responseKeys["success"] && $responseKeys["score"] >= 0.5) {
   echo json_encode(array('success' => 'true', 'om_score' => $responseKeys["score"], 'token' => $_POST['token']));
 } else {
   echo json_encode(array('success' => 'false', 'om_score' => $responseKeys["score"], 'token' => $_POST['token']));
